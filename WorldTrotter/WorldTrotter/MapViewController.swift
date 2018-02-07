6a822aa7cdfdb524645f00cb691a0aa53f410585
Sajjad Patel <sajjadp92@outlook.com>
2018-02-07 13:49:15 -0500

    Added segmented control Programmatically added the Map Added the segemented controls Put constraints on the segmented control Wired up the segmented controls to change the map depending on what the user clics

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,10 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController{
+    
+    //Create map view
+    let mapView = MKMapView()
+    
     override func viewDidLoad(){
         super.viewDidLoad()
-        print("MapViewController loaded its view.")
+        
+    }
+    
+    override func loadView(){
+        
+        //Set up as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["standard", "Hybrid", "Satillite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let margins = view.layoutMarginsGuide
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        
+        //Activate the constraints
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl){
+        switch segControl.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = .standard
+            break
+        case 1:
+            mapView.mapType = .hybrid
+            break
+        case 2:
+            mapView.mapType = .satellite
+            break
+        default:
+            break
+        }
     }
 }
