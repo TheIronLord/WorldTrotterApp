1657d2f8e141f864ad8bb0891bca850c1a6c7a67
Sajjad Patel <sajjadp92@outlook.com>
2018-02-18 14:07:34 -0500

    Gold Challenge: Dropping pins - Completed Dropped 3 annotation pins, one where I was born, one where I was revealed, and one final one wher my enemy was created ie Android

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,20 +12,68 @@ import CoreLocation
 
 class MapViewController: UIViewController{
     
-    //Create map view
-    let mapView = MKMapView()
-    var locationManager = CLLocationManager()
-    let authorizationStatus = CLLocationManager.authorizationStatus()
-    let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+    //Locally used variables
+    let mapView                 = MKMapView()
+    let authorizationStatus     = CLLocationManager.authorizationStatus()
+    let span: MKCoordinateSpan  = MKCoordinateSpanMake(0.01, 0.01)
     
+    var locationManager         = CLLocationManager()
+    var annotationLocation      = 0
+    
+    //UI Variables
     var showCurrentLocation: UIButton!
     var showAnnotationLocation: UIButton!
     
+    //Creates the MKAnnotationViews
+    func setAnnotationLocation(_ annotationLocation:Int){
+        switch annotationLocation{
+        case 1:
+            //Create annotation on where I was created
+            let location: CLLocationCoordinate2D    = CLLocationCoordinate2DMake(37.335170, -122.013552)
+            let region: MKCoordinateRegion          = MKCoordinateRegionMake(location, span)
+            mapView.setRegion(region, animated: true)
+            
+            let annotationCreator = MKPointAnnotation()
+            annotationCreator.coordinate            = location
+            annotationCreator.title                 = "Where I was Born"
+            annotationCreator.subtitle              = "This is where Tim (my creator) made me"
+            mapView.addAnnotation(annotationCreator)
+            break
+        case 2:
+            //Create annotation for where I was revealed to the world
+            let location: CLLocationCoordinate2D    = CLLocationCoordinate2DMake(37.331076, -122.007470)
+            let region: MKCoordinateRegion          = MKCoordinateRegionMake(location, span)
+            mapView.setRegion(region, animated: true)
+            
+            let annotationRevealing = MKPointAnnotation()
+            annotationRevealing.coordinate          = location
+            annotationRevealing.title               = "This is where Tim revealed me to the world"
+            annotationRevealing.subtitle            = "Steve Jobs Thearter"
+            mapView.addAnnotation(annotationRevealing)
+            break
+        case 3:
+            //Create annotation for where my enemy was created
+            let location: CLLocationCoordinate2D    = CLLocationCoordinate2DMake(37.422296, -122.084103)
+            let region: MKCoordinateRegion          = MKCoordinateRegionMake(location, span)
+            mapView.setRegion(region, animated: true)
+            
+            let annotationEnemy = MKPointAnnotation()
+            annotationEnemy.coordinate              = location
+            annotationEnemy.title                   = "My Enemy"
+            annotationEnemy.subtitle                = "The people who made my enemy... Android"
+            mapView.addAnnotation(annotationEnemy)
+            break
+        default:
+            break
+        }
+    }
     
     override func viewDidLoad(){
         super.viewDidLoad()
-        mapView.delegate = self
-        locationManager.delegate = self
+        
+        //Set the delegates and set up location services
+        mapView.delegate                = self
+        locationManager.delegate        = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         configureLocationServices()
@@ -38,7 +86,7 @@ class MapViewController: UIViewController{
         showAnnotationLocation = UIButton.init(type: UIButtonType.system)
         view = mapView
 
-        
+        //Create segmentedControl for the different types of mapViews
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
@@ -46,7 +94,17 @@ class MapViewController: UIViewController{
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
         
-        //Create Show User Location UIButton
+        //Add Constraints for segmentedControl
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        
+        //Activate the constraints for segmentedControl
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        //Create showCurrentLocation UIButton
         showCurrentLocation.setTitle("Show Current Location", for: .normal)
         showCurrentLocation.bounds = CGRect(x: 0, y: 0, width: 500, height: 100)
         showCurrentLocation.center = CGPoint(x: 200, y: 650)
@@ -56,19 +114,9 @@ class MapViewController: UIViewController{
         //Create showAnnotationLocation UIButton
         showAnnotationLocation.setTitle("Show Annotation Location", for: .normal)
         showAnnotationLocation.bounds = CGRect(x: 0, y: 0, width: 500, height: 100)
-        showAnnotationLocation.center = CGPoint(x: 200, y: 600)
+        showAnnotationLocation.center = CGPoint(x: 200, y: 550)
         showAnnotationLocation.addTarget(self, action: #selector(showAnnotionLocations(_:)), for: .touchUpInside)
         view.addSubview(showAnnotationLocation)
-        
-        //Add Constraints for segmentedControl
-        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
-        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
-        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        
-        //Activate the constraints
-        topConstraint.isActive = true
-        leadingConstraint.isActive = true
-        trailingConstraint.isActive = true
     }
     
     @objc func mapTypeChanged(_ segControl: UISegmentedControl){
@@ -95,7 +143,8 @@ class MapViewController: UIViewController{
     }
     
     @objc func showAnnotionLocations(_ showCurrentLocationButton: UIButton){
-        
+        annotationLocation = (annotationLocation + 1) % 4
+        setAnnotationLocation(annotationLocation)
     }
 }
 
